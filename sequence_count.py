4f02c4af002bd49eed9deff74bde8bacddbbb242
donggel <alan32.liu@gmail.com>
2021-05-04 17:17:57 +1000

    a Better way to construct aflnet tree

--- sequence_count.py
+++ sequence_count.py
@@ -22,56 +22,82 @@ def collect_sequence(result_dir):
     return sequences
 
 
-def collect_each_selection_stats(stats_file):
-    with open(stats_file, 'r') as stats_file:
-        for line in stats_file:
-            if line[:len("[SELECTION]")] != "[SELECTION]":
-                continue
-            states = [int(state) for state in line[:-1].split(":")[-1].split(" ") if state]
-            is_missing_from_log = True
-            for sequence in aflnet_sequences_log:
-                if states == sequence[:len(states)]:
-                    is_missing_from_log = False
-                    break
-            if is_missing_from_log:
-                print(f"Missing from log: {states}")
-                AFLNet_ROOT.add_trace(trace=states)
-
-            is_missing_from_dir = True
-            for sequence in aflnet_sequences_dir:
-                if states == sequence[:len(states)]:
-                    is_missing_from_dir = False
-                    break
-            if is_missing_from_dir:
-                print(f"Missing from dir: {states}")
-                # AFLNet_ROOT.add_trace(trace=states)
-
-            AFLNet_ROOT.record_selection_trace(trace=states)
-
-
-def collect_each_execution_stats(stats_file):
-    sequences = []
-    with open(stats_file, 'r') as stats_file:
-        for line in stats_file:
-            if line[:len("[Execution]")] != "[Execution]":
-                continue
-            states = [int(state) for state in line[:-1].split(":")[-1].split(" ") if state]
-            if states in sequences:
-                # print(f"Sequence exits: {states}")
-                continue
-            AFLNet_ROOT.add_trace(trace=states)
-            sequences.append(states)
-    return sequences
+# def collect_each_selection_stats(stats_file):
+#     with open(stats_file, 'r') as stats_file:
+#         for line in stats_file:
+#             if line[:len("[SELECTION]")] != "[SELECTION]":
+#                 continue
+#             states = [int(state) for state in line[:-1].split(":")[-1].split(" ") if state]
+#             is_missing_from_log = True
+#             for sequence in aflnet_sequences_log:
+#                 if states == sequence[:len(states)]:
+#                     is_missing_from_log = False
+#                     break
+#             if is_missing_from_log:
+#                 print(f"Missing from log: {states}")
+#                 AFLNet_ROOT.add_trace(trace=states)
+#
+#             is_missing_from_dir = True
+#             for sequence in aflnet_sequences_dir:
+#                 if states == sequence[:len(states)]:
+#                     is_missing_from_dir = False
+#                     break
+#             if is_missing_from_dir:
+#                 print(f"Missing from dir: {states}")
+#                 # AFLNet_ROOT.add_trace(trace=states)
+#
+#             AFLNet_ROOT.record_selection_trace(trace=states)
 
 
-aflnet_sequences_dir = collect_sequence(aflnet_dir)
+# def collect_each_execution_stats(stats_file):
+#     sequences = []
+#     with open(stats_file, 'r') as stats_file:
+#         for line in stats_file:
+#             if line[:len("[Execution]")] != "[Execution]":
+#                 continue
+#             states = [int(state) for state in line[:-1].split(":")[-1].split(" ") if state]
+#             if states in sequences:
+#                 # print(f"Sequence exits: {states}")
+#                 continue
+#             AFLNet_ROOT.add_trace(trace=states)
+#             sequences.append(states)
+#     return sequences
+
+
+def construct_aflnet_tree(aflnet_stats_file):
+    def record_execution():
+        if line[:len("[Execution]")] != "[Execution]":
+            return
+        cur_execution_path = [int(state) for state in line[:-1].split(":")[-1].split(" ") if state]
+        AFLNet_ROOT.record_simulation_trace(execution_trace=cur_execution_path,
+                                            selection_trace=cur_selection_path)
+        all_execution_path.append(cur_execution_path)
+
+    def record_selection():
+        nonlocal cur_selection_path
+        if line[:len("[SELECTION]")] != "[SELECTION]":
+            return
+        cur_selection_path = [int(state) for state in line[:-1].split(":")[-1].split(" ") if state]
+        AFLNet_ROOT.record_selection_trace(trace=cur_selection_path)
+
+    with open(aflnet_stats_file, 'r') as aflnet_stats:
+        cur_selection_path = [0]    # Assuming the credit of seed inputs goes to the root
+        all_execution_path = []
+        for line in aflnet_stats:
+            record_execution()
+            record_selection()
+    return all_execution_path
+
+
+
+
 
 
 AFLNet_ROOT: TreeNode = TreeNode(0)
 
-
-aflnet_sequences_log = collect_each_execution_stats(stats_file=aflnet_report)
-collect_each_selection_stats(stats_file=aflnet_report)
+aflnet_sequences_dir = collect_sequence(aflnet_dir)
+aflnet_sequences_log = construct_aflnet_tree(aflnet_stats_file=aflnet_report)
+# collect_each_selection_stats(stats_file=aflnet_report)
 
 
 only_in_log_counter = 0
