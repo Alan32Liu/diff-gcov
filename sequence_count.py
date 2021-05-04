596b2b921eddb3e607736dcbb6c08a757e40514d
donggel <alan32.liu@gmail.com>
2021-05-04 14:32:57 +1000

    Removing the old tree drawing functionality

--- sequence_count.py
+++ sequence_count.py
@@ -3,7 +3,7 @@ import os
 import pathlib
 import sys
 
-import pygraphviz as pg
+# import pygraphviz as pg
 
 sys.path.insert(0, f"{pathlib.Path(__file__).parent.parent.absolute()}/VisualiseTree")
 from visualise_tree import Node as TreeNode
@@ -52,47 +52,11 @@ def collect_each_selection_stats(stats_file):
             AFLNet_ROOT.record_selection_trace(trace=states)
 
 
-def draw_tree(sequences, tree_name):
-    def build_trace():
-        for i in range(1, len(sequence)):
-            print(Node(sequence[:i-1], sequence[i-1]), Node(sequence[:i], sequence[i]))
-            tree.add_edge(Node(sequence[:i-1], sequence[i-1]), Node(sequence[:i], sequence[i]))
-
-    tree = pg.AGraph(directed=True, strict=True)
-
-    for sequence in sequences:
-        build_trace()
-
-    tree.write('{}.dot'.format(tree_name))
-    tree.layout(prog='dot')
-
-
-class Node:
-    def __init__(self, sequence, name):
-        self.sequence = sequence
-        self.name = name
-
-    def __eq__(self, other: "Node"):
-        return self.sequence == other.sequence
-
-    def __repr__(self):
-        return self.name
-
-    def __str__(self):
-        # return (str(hash(tuple(self.sequence[:-1])))
-        #         + ","
-        #         + str(self.sequence[-1])) if self.sequence else "[]"
-        return str(self.name)
-
-
 aflnet_sequences = collect_sequence(aflnet_dir)
 legion_sequences = collect_sequence(legion_dir)
 
 AFLNet_ROOT: TreeNode = TreeNode(0)
 Legion_ROOT: TreeNode = TreeNode(0)
-
-# draw_tree(aflnet_sequences, "AFLNet_Tree")
-# draw_tree(legion_sequences, "Legion_Tree")
 
 for sequence in aflnet_sequences:
     AFLNet_ROOT.add_trace(trace=sequence)
